d50a37c0a64be5f25389535c73c6b77e96ccb36c
donglijie <673742128@qq.com>
2022-07-20 05:54:17 +0000

    增加最基本的命令行解析的操作

---
+++ step1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <argp.h>
+
+static int parse_opt (int key, char *arg, struct argp_state *state){
+  switch (key){
+        case 'd': printf (".\n"); break;
+    }
+  return 0;
+}
+
+int main (int argc, char **argv){
+    // 声明了结构体数组, argp_option 是结构体的名称
+    struct argp_option options[] = {
+      // 第一个选项 ，操作符是d， Show a dot on the screen是提示, 输入-- help的时候
+        { 0, 'd', 0, 0, "Show a dot on the screen"},
+      // 一个空的终止记录
+        { 0 }
+    };
+
+    struct argp argp = { options, parse_opt };
+    return argp_parse (&argp, argc, argv, 0, 0, 0);
+}
