19c1abf73898043e60adcf24054d51fe24717769
donglijie <673742128@qq.com>
2022-08-01 16:34:15 +0000

    增加多文件编译

--- multi-file/bar.c
+++ multi-file/bar.c
@@ -1,3 +1,4 @@
+#include "bar.h"
 int add(int x, int y) {
   return x + y;
 }
--- multi-file/foo.c
+++ multi-file/foo.c
@@ -1,6 +1,6 @@
 // File foo.c
 #include <stdio.h>
-
+#include "bar.h"
 int main(void) {
   printf("%d\n", add(2, 3));  // 5!
 }
