74d120f114fcda8c5310ad95f363a61d27164e92
donglijie <673742128@qq.com>
2022-08-07 03:53:08 +0000

    实现发送以太网数据，解析命令行参数

--- ethernet/send_ether.c
+++ ethernet/send_ether.c
@@ -1,4 +1,5 @@
 #include <argp.h>
+#include <stdio.h>
 /**
  * struct for storing command line arguments.
  * 结构体用于存放解析结果
@@ -47,5 +48,67 @@ static error_t opt_handler(int key, char *arg, struct argp_state *state) {
     return 0;
 }
 
+/**
+ * Parse command line arguments given by argc, argv.
+ *
+ *  Arguments
+ *      argc: the same with main function.
+ *
+ *      argv: the same with main function.
+ *
+ *  Returns
+ *      Pointer to struct arguments if success, NULL if error.
+ **/
+static struct arguments const *parse_arguments(int argc, char *argv[]) {
+    // docs for program and options
+    static char const doc[] = "send_ether: send data through ethernet frame";
+    static char const args_doc[] = "";
 
-int main(int argc, char *argv[]);
+    // command line options
+    static struct argp_option const options[] = {
+        // 第一个选项是命令行参数的长选项，第二个是参数的短选项，第三个选项 表示 该参数后面必须要跟一个 输入
+        // Option -i --iface: name of iface through which data is sent
+        {"iface", 'i', "IFACE", 0, "name of iface for sending"},
+
+        // Option -t --to: destination MAC address
+        {"to", 't', "TO", 0, "destination mac address"},
+
+        // Option -T --type: data type
+        {"type", 'T', "TYPE", 0, "data type"},
+
+        // Option -d --data: data to send, optional since default value is set
+        {"data", 'd', "DATA", 0, "data to send"},
+
+        { 0 }
+    };
+
+    static struct argp const argp = {
+        options,
+        opt_handler,
+        args_doc,
+        doc,
+        0,
+        0,
+        0,
+    };
+
+    // for storing results
+    static struct arguments arguments = {
+        .iface = NULL,
+        .to = NULL,
+        //default data type: 0x0900
+        .type = 0x0900,
+        // default data, 46 bytes string of 'a'
+        // since for ethernet frame data is 46 bytes at least
+        .data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    };
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    return &arguments;
+}
+int main(int argc, char *argv[]){
+    // parse command line options to struct arguments
+    struct arguments const *arguments = parse_arguments(argc, argv);
+    printf("data is:%s\n", arguments->data);
+}
