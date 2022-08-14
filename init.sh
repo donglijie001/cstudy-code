d2ea6aec15db65fa61ab73a1db322238f4dfc751
donglijie <673742128@qq.com>
2022-08-14 01:45:30 +0000

    增加code-server容器的初始化脚本

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# code-server容器初始化命令
+echo "alias gpl=\"git pull\"" >> ~/.bash_aliases
+echo "alias gp=\"git push\"" >> ~/.bash_aliases
+echo "alias gpu=\"git push -u origin\"" >> ~/.bash_aliases
+echo "alias gc-m=\"git commit -m\"" >> ~/.bash_aliases
