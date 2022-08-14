7caa65e4f1259c3116ea091473fe75e61a1f92c3
donglijie <673742128@qq.com>
2022-08-14 06:24:23 +0000

    增加写文件的代码

--- socket/low_read.c
+++ socket/low_read.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-/*
-* 写文件
-*/
+#define BUF_SIZE 100
 void error_handling(char* message){
     fputs(message, stderr);
     fputc('\n', stderr);
@@ -12,15 +10,16 @@ void error_handling(char* message){
 }
 int main(void){
     int fd;
-    char buf[]= "Let's go!\n";
-    fd=open("data.txt",O_CREAT|O_WRONLY|O_TRUNC);
+    char buf[BUF_SIZE];
+    fd= open("data.txt", O_RDONLY);
     if (fd==-1) {
-        error_handling("open() error");
+        error_handling("opne() error!");
     }
-    printf("file descriptor:%d\n",fd);
-    if (write(fd,buf,sizeof(buf))==-1) {
+    printf("file descripter:%d \n", fd);
+    if (read(fd,buf,sizeof(buf))==-1) {
         error_handling("write() error");
     }
+    printf("file data:%s", buf);
     close(fd);
     return 0;
 }
