37fb81e27b8f5f996025351c6dbd7b8d8dfdc74f
donglijie <673742128@qq.com>
2022-08-17 05:47:43 +0000

    增加根据域名获取主机信息代码

---
+++ socket/ch08/gethostbyname.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+void error_handling(char *message);
+
+int main(int argc,char* argv[]){
+    int i;
+    struct hostent *host;
+    if (argc!=2) {
+        printf("Usage:%s <port>\n",argv[0]);
+        exit(1);
+    }
+    host=gethostbyname(argv[1]);
+    if (!host) {
+        error_handling("gethost...error");
+    }
+    printf("Official name:%s \n",host->h_name);
+}
+void error_handling(char* message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
