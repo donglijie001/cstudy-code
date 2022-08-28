bf1580f60dfc6c2845f85ed7b34e4aa8acc6f2c2
donglijie <673742128@qq.com>
2022-08-28 18:06:26 +0800

    增加ip数据报的笔记

--- socket/ch09/sock_type.c
+++ socket/ch09/sock_type.c
@@ -14,6 +14,17 @@ int main(int argc, char* argv[]){
     udp_sock = socket(PF_INET, SOCK_DGRAM, 0);
     printf("SOCK_STREAM: %d\n", SOCK_STREAM);
     printf("SOCK_STREAM: %d\n", SOCK_DGRAM);
+    state = getsockopt(tcp_sock, SOL_SOCKET, SO_TYPE, (void*)&sock_type, &optlen);
+    if (state) {
+        error_handling("getsockopt error");
+    }
+    printf("Socket type:one:%d\n", sock_type);
+    state = getsockopt(udp_sock, SOL_SOCKET, SO_TYPE, (void*)&sock_type, &optlen);
+    if (state) {
+        error_handling("getsockopt error");
+    }
+    printf("Socket type:two:%d\n", sock_type);
+
     return 0;
 }
 
