ac2c4a91c203028771cdbdb19c33b429bf4f7661
donglijie <673742128@qq.com>
2022-09-16 08:52:54 +0800

    update

--- uniux-linux/ch01/more01.c
+++ uniux-linux/ch01/more01.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define PAGELEN 24
-#define LINELEN 512
+#define PAGELEN 1
+#define LINELEN 12
 void do_more(FILE *);
 int see_more();
 int main (int ac,char* av[]){
     FILE * fp;
+    printf("argument count:%d\n", ac);
     if (ac==1) {
+        // 如果参数只有一个就，就走这段逻辑，./more01.o 只运行这个程序就是一个参数
+        printf("%s\n", av[0]);
         do_more(stdin);
     }else {
+        printf("%s\n", av[1]);
+        // 这个是判断命令行中携带的参数是否真的是超过了1个，但是我感觉外面有if判断
+        // 似乎不需要这个。
         while (--ac) {
+            //av里面存储的是命令行执行的参数，第二个参数就是执行的文件名
             if ((fp=fopen(* ++av,"r"))!=NULL) {
                 do_more(fp);
                 fclose(fp);
@@ -23,6 +30,7 @@ int main (int ac,char* av[]){
 void do_more(FILE *fp){
     char line[LINELEN];
     int num_of_lines =0;
+    // 这里只是声明see_more函数
     int see_more(),reply;
     while(fgets(line, LINELEN, fp)){
         if (num_of_lines == PAGELEN) {
