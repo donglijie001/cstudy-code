c30f4ea53d939ecabd326584c8dd61b6a9b9ec3e
donglijie <673742128@qq.com>
2022-12-24 20:07:33 +0800

    update

--- c_game_2048/game_2048.c
+++ c_game_2048/game_2048.c
@@ -23,18 +23,25 @@ int main(){
     endwin();
     return 0;
 }
-void init(){
-    int x,y;
+void init()
+{
+    int x, y;
+
     initscr();
     cbreak();
+    noecho();
+    curs_set(0);
 
+    empty = 15;
+    srand(time(0));
+    x = rand() % 4;
+    y = rand() % 4;
+    a[y][x] = 2;
+    draw();
 }
 void play(){
 
 }
-void draw_one(int y, int x){
-
-} 
 void cnt_value(int *new_y, int *new_x){
 
 }
@@ -43,4 +50,57 @@ int game_over(){
 }
 int cnt_one(int y, int x){
     return 0;
+}
+void draw()
+{
+    int n, m, x, y;
+    char c[4] = {'0', '0', '0', '0'};
+    clear();
+
+    for(n = 0; n < 9; n += 2)   //绘制横线，n代表行，m代表列
+    {
+        for(m = 0; m < 21; m++)
+        {
+            move(n, m);
+            addch('-');
+            refresh();
+        }
+    }
+    for(m = 0; m < 22; m += 5)  //绘制竖线
+    {
+        for(n = 1; n < 8; n++)
+        {
+            move(n, m);
+            addch('|');
+            refresh();
+        }
+    }
+    for(y = 0; y < 4; y++)     //绘制数字
+    {
+        for(x = 0; x < 4; x++)
+        {
+            draw_one(y, x);
+        }
+    }
+}
+void draw_one(int y, int x)
+{
+    int i, m, k, j;
+    char c[5] = {0x00};
+    i = a[y][x];
+    m = 0;
+    while(i > 0)
+    {
+        j = i % 10;
+        c[m++] = j + '0';
+        i = i / 10;
+    }
+    m = 0;
+    k = (x + 1) * 5 - 1;
+    while(c[m] != 0x00)
+    {
+        move(2*y+1, k);
+        addch(c[m++]);
+        k--;
+    }
 }
